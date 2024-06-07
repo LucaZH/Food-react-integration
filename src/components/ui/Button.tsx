21131deea260434dd47705740eb8abbf18e0ff59
lucaZH <lucarandria1@gmail.com>
2024-06-07 09:14:03 +0300

    Add Button component

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,8 @@
+function Button({ text }: { text: string }) {
+  return (
+    <button className="rounded-3xl text-white font-semibold bg-active py-2 px-4 drop-shadow-md hover:bg-transparent hover:text-active hover:border hover:border-active">
+      {text}
+    </button>
+  );
+}
+export default Button;
