1fe25dbf9cabb8e38ff0ffdd385c2d98b6529d94
lucaZH <lucarandria1@gmail.com>
2024-06-07 09:13:43 +0300

    Add active color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        active:"#FF5331"
+      }
+    },
   },
   plugins: [],
 }
