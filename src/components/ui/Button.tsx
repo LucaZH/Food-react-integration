b9ea3b86083931cceed18d9e91ab5825ca60e9c4
lucaZH <lucarandria1@gmail.com>
2024-06-07 09:18:20 +0300

    Refactor Button component to use FC type

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,8 +1,11 @@
-function Button({ text }: { text: string }) {
+import { FC } from "react";
+
+const Button: FC<{ text: string }> = ({ text }) => {
   return (
     <button className="rounded-3xl text-white font-semibold bg-active py-2 px-4 drop-shadow-md hover:bg-transparent hover:text-active hover:border hover:border-active">
       {text}
     </button>
   );
-}
+};
+
 export default Button;
