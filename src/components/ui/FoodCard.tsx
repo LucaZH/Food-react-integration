85684edd56ab475bd95872595bf1f196304b29be
lucaZH <lucarandria1@gmail.com>
2024-06-08 20:34:19 +0300

    Add styles

--- src/components/ui/FoodCard.tsx
+++ src/components/ui/FoodCard.tsx
@@ -7,12 +7,12 @@ const FoodCard: FC<{
   imgUrl: string;
 }> = ({ foodName, price, link, imgUrl }) => {
   return (
-    <div className="relative w-60 h-60">
-      <img src={imgUrl} alt={foodName} className="absolute" />
-      <div className="relative w-full h-full bottom-0">
-        <h4 className="text-2xl">{foodName}</h4>
-        <p className="text-lg">${price}</p>
-        <a href={link}>Click Now</a>
+    <div className="relative">
+      <img src={imgUrl} alt={foodName} className="rounded-3xl" />
+      <div className="absolute bottom-0 pl-8 pb-6 text-white font-black">
+        <h4 className="text-3xl">{foodName}</h4>
+        <p className="text-3xl">${price}</p>
+        <a href={link}>Click Now </a>
       </div>
     </div>
   );
