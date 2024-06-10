aaec7b23bdab1f5a48d7da82e372334f9d6e71bc
lucaZH <lucarandria1@gmail.com>
2024-06-10 11:52:33 +0300

    Add boder active to the div

--- src/components/ui/FoodItem.tsx
+++ src/components/ui/FoodItem.tsx
@@ -6,13 +6,15 @@ const FoodItem: FC<{ imgUrl: string; name: string; active: boolean }> = ({
   active,
 }) => {
   return (
-    <div
-      className={`flex items-center rounded-full py-2 px-2 gap-5 ${
-        active ? "bg-active text-white" : "bg-white text-black"
-      }`}
-    >
-      <img src={imgUrl} alt={name} className="rounded-full w-10 h-10" />
-      <p className="text-lg font-semibold">{name}</p>
+    <div className={`pr-8 -mr-1 ${active ? "border-r-4 border-r-active" : ""}`}>
+      <div
+        className={`flex items-center rounded-full py-2 px-2 gap-5 pr-10 ${
+          active ? "bg-active text-white" : "bg-white text-black"
+        }`}
+      >
+        <img src={imgUrl} alt={name} className="rounded-full w-10 h-10" />
+        <p className="text-lg font-semibold">{name}</p>
+      </div>
     </div>
   );
 };
