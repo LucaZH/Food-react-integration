3be64e32957deb804135e71908d6d51d35e478a2
lucaZH <lucarandria1@gmail.com>
2024-06-16 18:34:56 +0300

    Add PubBanner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero";
 import Menu from "./components/Menu";
 import NavBar from "./components/Navbar";
+import PubBanner from "./components/PubBanner";
 import Service from "./components/Service";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Service />
       <Menu />
+      <PubBanner />
     </>
   );
 }
---
+++ src/components/PubBanner.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const PubBanner: FC = () => {
+  return (
+    <img
+      src="/public/img/plat.jpg"
+      alt="PubBanner"
+      className="w-full h-72 my-10 px-16"
+    />
+  );
+};
+export default PubBanner;
