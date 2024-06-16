f1c5efacd590ee0e87bbd530ad74805f8d936000
lucaZH <lucarandria1@gmail.com>
2024-06-16 19:32:47 +0300

    Fix quote size

--- src/components/ui/TestmonialCard.tsx
+++ src/components/ui/TestmonialCard.tsx
@@ -43,7 +43,7 @@ const TestmonialCard: FC<{
           viewBox="0 0 16 16"
           version="1.1"
           xmlns="http://www.w3.org/2000/svg"
-          className="w-6 h-6 fill-active"
+          className="w-14 h-14 fill-active"
         >
           <path d="M16,3V1c-4.188,0-7,2.812-7,7v7h7V8h-5C11,4.916,12.916,3,16,3z M0,8v7h7V8H2c0-3.084,1.916-5,5-5V1C2.812,1,0,3.812,0,8z" />
         </svg>
